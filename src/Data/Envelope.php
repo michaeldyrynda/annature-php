60d9adc7b77e3a6f9a44480806449be4e0272c09
Michael Dyrynda <michael@dyrynda.com.au>
2024-03-17 11:00:33 +1030

    restore envelope as a response mapping object

--- src/Data/Envelope.php
+++ src/Data/Envelope.php
@@ -6,33 +6,28 @@ namespace Dyrynda\Annature\Data;
 
 use DateTimeImmutable;
 use Dyrynda\Annature\Enum\EnvelopeStatus;
-use Illuminate\Support\Collection;
 
 final readonly class Envelope extends Data
 {
     public function __construct(
+        public string $id,
         public string $name,
+        public string $message,
+        public EnvelopeStatus $status,
+        public bool $shared,
+        public DateTimeImmutable $created,
+        public ?DateTimeImmutable $sent,
+        public ?DateTimeImmutable $voided,
+        public ?DateTimeImmutable $declined,
+        public ?DateTimeImmutable $completed,
+        public ?string $master,
+        public ?string $certificate,
         public string $accountId,
+        public ?string $groupId,
         /** @var \App\Data\Recipient[] */
-        public Collection $recipients,
-        public ?string $id = null,
-        public ?string $message = null,
-        public ?EnvelopeStatus $status = null,
-        public ?DateTimeImmutable $created = null,
-        public ?DateTimeImmutable $sent = null,
-        public ?DateTimeImmutable $voided = null,
-        public ?DateTimeImmutable $declined = null,
-        public ?DateTimeImmutable $completed = null,
-        public ?string $original = null,
-        public ?string $master = null,
-        public ?string $certificate = null,
-        public ?string $groupId = null,
-        public ?bool $shared = null,
-        public ?bool $draft = null,
-        /** @var array<array-key, \App\Data\Document>|null */
-        public ?Collection $documents = null,
+        public array $recipients,
         /** @var array<string, mixed> */
-        public array $metadata = [],
+        public array $metadata
     ) {
     }
 
@@ -42,19 +37,18 @@ final readonly class Envelope extends Data
             id: $properties['id'],
             name: $properties['name'],
             message: $properties['message'],
-            status: EnvelopeStatus::tryFrom($properties['status'] ?? ''),
+            status: EnvelopeStatus::from($properties['status']),
             shared: $properties['shared'],
-            created: $properties['created'] ? DateTimeImmutable::createFromFormat('Y-m-d\TH:i:s.v\Z', $properties['created']) : null,
+            created: DateTimeImmutable::createFromFormat('Y-m-d\TH:i:s.v\Z', $properties['created']),
             sent: $properties['sent'] ? DateTimeImmutable::createFromFormat('Y-m-d\TH:i:s.v\Z', $properties['sent']) : null,
             voided: $properties['voided'] ? DateTimeImmutable::createFromFormat('Y-m-d\TH:i:s.v\Z', $properties['voided']) : null,
             declined: $properties['declined'] ? DateTimeImmutable::createFromFormat('Y-m-d\TH:i:s.v\Z', $properties['declined']) : null,
             completed: $properties['completed'] ? DateTimeImmutable::createFromFormat('Y-m-d\TH:i:s.v\Z', $properties['completed']) : null,
-            original: $properties['original'] ?? null,
             master: $properties['master'] ?? null,
             certificate: $properties['certificate'] ?? null,
             accountId: $properties['account_id'],
             groupId: $properties['group_id'],
-            recipients: collect($properties['recipients'])->map(fn (array $recipient) => Recipient::fromArray($recipient)),
+            recipients: $properties['recipients'],
             metadata: $properties['metadata'] ?? [],
         );
     }
